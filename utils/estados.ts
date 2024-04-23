4d515dd1452e90aead733c079d9752e7f89e4997
Anderson Marlon <anderson18.marlon@gmail.com>
2024-04-23 11:20:06 -0300

    Removed: Jest + Added: Bun:test + Test file - 23-04 11:20

--- utils/estados.ts
+++ utils/estados.ts
@@ -1,17 +1,17 @@
 interface Estado {
     id: string;
     nome: string;
-  }
-  
-  interface Cidade {
+}
+
+interface Cidade {
     siglaEstado: string;
     nome: string;
-  }
-  
-  interface Database {
+}
+
+interface Database {
     estados: Estado[];
     cidades: Cidade[];
-  }
+}
   
   function Brasil(): Database {
     return require('./database.json');
